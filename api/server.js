bc0b4341f49f3d2bae49aaf0cc7e0c87e360fd34
Merwan-J <merwanj71@gmail.com>
2023-09-30 12:15:15 +0300

    deployment config

--- api/server.js
+++ api/server.js
@@ -1,10 +1,18 @@
-const jsonServer = require("json-server"); // importing json-server library
+// See https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
+server.use(
+    jsonServer.rewriter({
+        "/api/*": "/$1",
+    })
+);
 server.use(router);
+server.listen(8080, () => {
+    console.log("JSON Server is running");
+});
 
-server.listen(port);
+module.exports = server;
